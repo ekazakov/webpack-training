320cf160d6c2c78d9107af5e641a3b6fbc853d6c
Evgeniy Kazakov <evgeniy.kazakov@gmail.com>
2019-07-02 23:51:01 +0200

    WIP SAVEPOINT

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 /* eslint-disable */
 
 import { Foo } from './Foo';
+import { check } from './utils';
 
 function foo() {
     console.log('foo');
 }
 
-if (process.env.NODE_ENV === 'production') {
+if (check()) {
     console.log('doo');
     const foo = new Foo();
     foo.do();
---
+++ src/utils.js
@@ -0,0 +1,1 @@
+export const check = () => process.env.NODE_ENV === 'production';
